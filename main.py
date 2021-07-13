eaa7aa526e0265ef5cc45abfec5a851a7503cb21
Divya Sirigiri <divyasirigiri7@gmail.com>
2021-07-13 09:48:40 +0530

    commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from logging import debug
+from flask import Flask, render_template, request
+import nltk
+from nltk import FreqDist
+from textblob import TextBlob
+from textblob import Word
+
+app1 = Flask(__name__)
+
+
+@app1.route('/')
+def hello():
+    return render_template('base.html')
+
+@app1.route('/define',methods = ['POST'])
+def define():
+    ip_word = request.form.get('ip-word')
+    text = TextBlob(ip_word)
+    word_count = len(text.words)
+    sen_count = len(text.sentences)
+    freq = FreqDist(nltk.word_tokenize(ip_word))
+    new = {}
+    for i in text.words:
+        if i not in new:
+            new[i] = freq[i]
+
+
+    return render_template('base.html',translated_text=f'\n the word count of "{(ip_word)}"  is :  {word_count} \n the sentence count is : {sen_count} \n the key word density is : {new}')
+
+if __name__=='__main__':
+    app1.run(debug=True)
