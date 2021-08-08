8df4cbacd6feb473fb9aaa27d8edd943a7817fb1
Divya Sirigiri <divyasirigiri7@gmail.com>
2021-08-08 10:53:00 +0530

    update

--- main.py
+++ main.py
@@ -15,18 +15,31 @@ def hello():
 @app1.route('/define',methods = ['POST'])
 def define():
     ip_word = request.form.get('ip-word')
+    ip2 = request.form.get('ip2')
     text = TextBlob(ip_word)
-    word_count = len(text.words)
-    sen_count = len(text.sentences)
+    
+    
     freq = FreqDist(nltk.word_tokenize(ip_word))
     new = {}
-    for i in text.words:
-        if i not in new:
-            new[i] = freq[i]
-    for i in new:
-        new[i] = str(new[i])+"("+str(round((new[i]/word_count)*100,2))+"%"+")"
+    if ip2=="Keyword Density":
+        word_count = len(text.words)
+        for i in text.words:
+            if i not in new:
+                new[i] = freq[i]
+        for i in new:
+            new[i] = str(round((new[i]/word_count)*100,2))+"%"
+        res = new
+    elif ip2=="Word Count":
+        word_count = len(text.words)
+        res=word_count
 
-    return render_template('base.html',translated_text=f'\n the word count of "{(ip_word)}"  is :  {word_count} \n the sentence count is : {sen_count} \n the key word density is : {new}')
+    elif ip2=="Sentence Count":
+        sen_count = len(text.sentences)
+        res = sen_count
+    elif ip2=="Character Count":
+        res = len(ip_word)
+
+    return render_template('base.html',translated_text=f'\n the "{(ip2)}" in the given text is :  {res}')
 
 if __name__=='__main__':
     app1.run(debug=True)
